98980d818e35f4610f5f9c8b2ddb25b16f2fe516
dayshawn terry <dayshawn21@gmail.com>
2021-10-04 12:57:11 -0500

    font

--- components/Header.js
+++ components/Header.js
@@ -9,30 +9,37 @@ const Header = () => {
 
         <h1>Frontend Developer / &#10095;</h1>
       </div>
-      <div className="py-2 text-center">
-        <a
-          href="https://github.com/Dayshawn21"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image src="/image/github.png" alt="" width={500} height={500} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/dayshawn-terry/"
-          rel="noopener noreferrer"
-          target="_blank"
-          className="mx-4"
-        >
-          <Image
-            src="/image/linkedin-app.png"
-            alt=""
-            width={500}
-            height={500}
-          />
-        </a>
-        <a href="dayshawn2.pdf" rel="noopener noreferrer" target="_blank">
-          <Image src="/image/resume.png" width={500} height={500} />
-        </a>
+
+      <div className="py-2 grid-3 text-center">
+        <div>
+          <a
+            href="https://github.com/Dayshawn21"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <Image src="/image/github.png" alt="" width={500} height={500} />
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/dayshawn-terry/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="mx-4"
+          >
+            <Image
+              src="/image/linkedin-app.png"
+              alt=""
+              width={500}
+              height={500}
+            />
+          </a>
+        </div>
+        <div>
+          <a href="dayshawn2.pdf" rel="noopener noreferrer" target="_blank">
+            <Image src="/image/resume.png" width={500} height={500} />
+          </a>
+        </div>
       </div>
     </header>
   );
